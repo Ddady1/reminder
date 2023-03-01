6642639eaf443e3006291f19be10c0e4d775bb78
Dady <ddady1@gmail.com>
2023-03-01 22:31:43 +0200

    Connection established

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from getpass import getpass
 from mysql.connector import connect, Error
 import json
+import pymysql
+
 
 
 
@@ -8,51 +10,15 @@ class Mysql:
     def __init__(self):
 
         try:
+
             with connect(
-                #host="localhost",
-                host=secret.get('aws_sql_address'),
-                user=input("Enter username: "),
-                #password=getpass("Enter password: "),
-                password='Aa1234',
-                database='test' # If data base exists
+                    host=config.get('aws_sql_address'),
+                    user=config.get('db_username'),
+                    # password=getpass("Enter password: "),
+                    password=config.get('db_pass'),
+                    # database=config.get('db_name')
             ) as connection:
-                print('Connection with database was established')
-
-                # Create DB
-
-                db = connection.cursor()
-                db.execute('show databases')
-                lst = db.fetchall()
-                print(len(lst))
-                if dbname in lst:
-                
-                    print('Database already exists!!!')
-                else:
-                    create_db_query = 'CREATE DATABASE test'
-                    with connection.cursor() as cursor:
-                        cursor.execute(create_db_query)
-                        print('Database was created')
-
-                # Create Licenses Table
-                create_licenses_table = '''
-                            '''CREATE TABLE IF NOT EXISTS licenses(
-                            id INT AUTO_INCREMENT PRIMARY KEY,
-                            product_name VARCHAR(100),
-                            issuer VARCHAR(100),
-                            license_id VARCHAR(100),
-                            purchase_date DATE,
-                            start_date DATE,
-                            end_date DATE,
-                            invoice VARCHAR(50),
-                            quantity INT(10),
-                            authorization_id VARCHAR(100),
-                            agreement_id VARCHAR(100)
-                            )
-                            '''
-                with connection.cursor() as cursor:
-                    cursor.execute(create_licenses_table)
-                    connection.commit()
-                    print('Table was created')
+                print('Connection with MySQL server was established')
 
 
 
@@ -95,6 +61,9 @@ class Mysql:
 
 
 if __name__ == '__main__':
+
+    with open('secret.json', 'r') as f:
+        config = json.load(f)
 
     mysql = Mysql()
 
