b61b9de43e8a992eaacbccb718f35c49f76024a5
Dady <ddady1@gmail.com>
2023-03-20 22:35:25 +0200

    Managed to pass connection to function. Working on delete db

--- sql_connection.py
+++ sql_connection.py
@@ -1,5 +1,7 @@
 import time
 
+import mysql.connector
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo, askyesno
@@ -20,7 +22,7 @@ def connect_sql():
     with open('assets/secret.json') as f:
         config = json.load(f)
 
-    try:
+    '''try:
         with connect(
                 host=config.get('sql_address'),
                 user=config.get('db_username'),
@@ -36,7 +38,21 @@ def connect_sql():
             #print(e)
             showinfo(title='Connection error.', message=messages(5))
 
-    return con
+    return con'''
+
+    connection = None
+    try:
+        connection = mysql.connector.Connect(
+            host=config.get('sql_address'),
+            user=config.get('db_username'),
+            password=config.get('db_pass')
+        )
+        progress(10, 60, 1)
+
+    except Error as e:
+        showinfo(title='Connection error.', message=messages(5))
+
+    return connection
 
 
 def progress(xval, yval, msg):
@@ -65,7 +81,9 @@ def messages(val):
     elif val == 5:
         return 'Could not connect with MySQL. Please check connection details and try again.'
     elif val == 6:
-        return 'Database by this name already exists'
+        return 'Database by this name already exists.\nWould like to overwrite it?\nCAUTION: Deleting the database will erase permanently all data in it!'
+    elif val == 7:
+        return 'Checking if Database already exists:'
 
 def check_db_exist(con):
 
@@ -75,8 +93,11 @@ def check_db_exist(con):
     i = 0
     while i < len(lst):
         if config.get('db_dbName') in lst[i]:
-            showinfo(title='DB status', message=messages(6))
-            # check tables existance code
+            answer = askyesno(title='DB status', message=messages(6))
+            if answer:
+                print('Deleting db')
+                #db.execute('DROP DATABASE ' + config.get('db_dbName')) # NEED TO UNTAG THIS LINE ON PRODUCTION
+                # check tables existance code
         else:
             i += 1
     #create_db()
@@ -105,6 +126,11 @@ root.iconbitmap('assets/reminder.ico')
 
 sql_connection_label = ttk.Label(root, text=messages(4))
 sql_connection_label.place(x=10, y=30)
+
+# PB checking database existence label
+
+db_exist_label = ttk.Label(root, text=messages(7))
+db_exist_label.place(x=10, y=90)
 
 
 # progressbar
