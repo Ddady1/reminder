21cc17e87591a3d5faaa00cfe3b03b2798d7804d
Dady <ddady1@gmail.com>
2023-03-21 22:02:56 +0200

    continue to table creation

--- sql_connection.py
+++ sql_connection.py
@@ -55,6 +55,14 @@ def connect_sql():
 
     return connection
 
+def progress_fake():
+    pb['value'] = 0
+    while pb['value'] < 100:
+        pb['value'] += 5
+        root.update_idletasks()
+        time.sleep(0.2)
+
+
 
 def progress(xval, yval, msg, color):
     pb['value'] = 0
@@ -98,39 +106,51 @@ def messages(val):
     elif val == 7:
         return 'Checking if Database already exists:'
     elif val == 8:
-        return 'Deleting old Database'
+        return 'Old Database was deleted'
     elif val == 9:
         return 'Database already exists!!!'
+    elif val == 10:
+        return 'Creating Database:'
 
 def check_db_exist(con):
 
     progressbar_position(230, 90, 300)
-    progress(10, 120, 9, 'red')
+    #progress(10, 120, 9, 'red')
+    progress_fake()
     db = con.cursor()
     db.execute('show databases')
     lst = db.fetchall()
     i = 0
     while i < len(lst):
         if config.get('db_dbName') in lst[i]:
+            result_labels_position(10, 120, 9, 'red')
             #global dbExist
             #dbExist = True
             answer = askyesno(title='DB status', message=messages(6))
             if answer:
                 db.execute('DROP DATABASE ' + config.get('db_dbName')) # NEED TO UNTAG THIS LINE ON PRODUCTION
+                result_labels_position(10, 150, 8, 'red')
+                create_db(con)
+                break
+
                 # check tables existance code
+            else:
+                break
         else:
             i += 1
-    #create_db()
+    #create_db(con)
 
 
-def create_db():
+def create_db(con):
+
     dbname = config.get('db_dbName')
     create_db_query = f'CREATE DATABASE {dbname}'
 
     try:
         with con.cursor() as cursor:
             cursor.execute(create_db_query)
-            progress(10, 120, 2)
+            progressbar_position(230, 180, 300)
+            progress(10, 210, 2, 'green')
     except Error as e:
         showinfo(title='Error', message=e)
 
@@ -151,6 +171,11 @@ sql_connection_label.place(x=10, y=30)
 
 db_exist_label = ttk.Label(root, text=messages(7))
 db_exist_label.place(x=10, y=90)
+
+# PB creating DB
+
+db_create_label = ttk.Label(root, text=messages(10))
+db_create_label.place(x=10, y=180)
 
 
 # progressbar
