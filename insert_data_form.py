35551a2ef5d76c14d7018c5228f032c03d7802d8
Ddady1 <ddady1@gmail.com>
2023-03-29 14:35:09 +0300

    date form design cont.

--- insert_data_form.py
+++ insert_data_form.py
@@ -9,12 +9,13 @@ import subprocess
 
 windll.shcore.SetProcessDpiAwareness(1)
 
+text_color = 'DodgerBlue4'
 
 # Create the main window
 
 root = tk.Tk()
 root.title('Reminder-Data Form')
-root.geometry('700x500+250+250')
+root.geometry('450x700+250+250')
 root.resizable(False, False)
 root.iconbitmap('assets/reminder.ico')
 
@@ -37,95 +38,137 @@ person_email = tk.StringVar()
 person_mobile = tk.StringVar()
 
 
+# main window label
+
+main_label = ttk.Label(root, text='Licenses Information Form', foreground=text_color, font=('Ariel', 14, 'bold'))
+main_label.place(x=100, y=10)
+
+
 # product name
 
-product_label = ttk.Label(root, text='Product Name:', foreground='blue', font=('Ariel', 10))
-product_label.place(x=20, y=10)
+product_label = ttk.Label(root, text='Product Name:', foreground=text_color, font=('Ariel', 10))
+product_label.place(x=20, y=50)
 
 product_entry = ttk.Entry(root, textvariable=product_name)
-product_entry.place(x=115, y=10, width=200)
+product_entry.place(x=20, y=70, width=300)
 product_entry.focus()
 
 
 # manufacture name
 
-manufacture_label = ttk.Label(root, text='Manufacturer Name:', foreground='blue', font=('Ariel', 10))
-manufacture_label.place(x=355, y=10)
+manufacture_label = ttk.Label(root, text='Manufacturer Name:', foreground=text_color, font=('Ariel', 10))
+manufacture_label.place(x=20, y=100)
 
 manufacture_entry = ttk.Entry(root, textvariable=manufacturer)
-manufacture_entry.place(x=480, y=10, width=200)
+manufacture_entry.place(x=20, y=120, width=300)
 
 
 # supplier name
 
-supplier_label = ttk.Label(root, text='Supplier Name:', foreground='blue', font=('Ariel', 10))
-supplier_label.place(x=20, y=100)
+supplier_label = ttk.Label(root, text='Supplier Name:', foreground=text_color, font=('Ariel', 10))
+supplier_label.place(x=20, y=150)
 
 supplier_entry = ttk.Entry(root, textvariable=supplier_name)
-supplier_entry.place(x=115, y=100)
+supplier_entry.place(x=20, y=170)
 
 
 # start date
 
-start_date_label = ttk.Label(root, text='Start Date:', foreground='blue', font=('Ariel', 10))
-start_date_label.place(x=300, y=100)
+start_date_label = ttk.Label(root, text='Start Date:', foreground=text_color, font=('Ariel', 10))
+start_date_label.place(x=20, y=200)
 
 start_date_entry = ttk.Entry(root, textvariable=start_date)
-start_date_entry.place(x=370, y=100, width=80)
+start_date_entry.place(x=20, y=220, width=80)
 
 
 # expiration date
 
-expired_date_label = ttk.Label(root, text='Expiration Date:', foreground='blue', font=('Ariel', 10))
-expired_date_label.place(x=500, y=100)
+expired_date_label = ttk.Label(root, text='Expiration Date:', foreground=text_color, font=('Ariel', 10))
+expired_date_label.place(x=200, y=200)
 
 expired_date_entry = ttk.Entry(root, textvariable=exp_date)
-expired_date_entry.place(x=600, y=100, width=80)
+expired_date_entry.place(x=200, y=220, width=80)
 
 
 # invoice number
 
-inv_num_label = ttk.Label(root, text='Invoice Number:', foreground='blue', font=('Ariel', 10))
-inv_num_label.place(x=20, y=190)
+inv_num_label = ttk.Label(root, text='Invoice Number:', foreground=text_color, font=('Ariel', 10))
+inv_num_label.place(x=20, y=250)
 
 inv_num_entry = ttk.Entry(root, textvariable=invoice_no)
-inv_num_entry.place(x=117, y=190)
+inv_num_entry.place(x=20, y=270)
 
 
 # quantity
 
-qty_label = ttk.Label(root, text='Quantity:', foreground='blue', font=('Ariel', 10))
-qty_label.place(x=310, y=190)
+qty_label = ttk.Label(root, text='Quantity:', foreground=text_color, font=('Ariel', 10))
+qty_label.place(x=20, y=300)
 
 qty_entry = ttk.Entry(root, textvariable=qty)
-qty_entry.place(x=370, y=190, width=80)
+qty_entry.place(x=20, y=320, width=80)
 
 
 # invoice date
 
-inv_date_label = ttk.Label(root, text='Invoice Date:', foreground='blue', font=('Ariel', 10))
-inv_date_label.place(x=520, y=190)
+inv_date_label = ttk.Label(root, text='Invoice Date:', foreground=text_color, font=('Ariel', 10))
+inv_date_label.place(x=20, y=350)
 
 inv_date_entry = ttk.Entry(root, textvariable=invoice_date)
-inv_date_entry.place(x=600, y=190, width=80)
+inv_date_entry.place(x=20, y=370, width=80)
 
 
 # license number
 
-lic_number_label = ttk.Label(root, text='License Number:', foreground='blue', font=('Ariel', 10))
-lic_number_label.place(x=20, y=280)
+lic_number_label = ttk.Label(root, text='License Number:', foreground=text_color, font=('Ariel', 10))
+lic_number_label.place(x=20, y=400)
 
 lic_number_entry = ttk.Entry(root, textvariable=lic_no)
-lic_number_entry.place(x=123, y=280, width=200)
+lic_number_entry.place(x=20, y=420, width=300)
 
 
 # authorization number
 
-auth_no_label = ttk.Label(root, text='Authorization Number:', foreground='blue', font=('Ariel', 10))
-auth_no_label.place(x=345, y=280)
+auth_no_label = ttk.Label(root, text='Authorization Number:', foreground=text_color, font=('Ariel', 10))
+auth_no_label.place(x=20, y=450)
 
 auth_no_entry = ttk.Entry(root, textvariable=autho_no)
-auth_no_entry.place(x=480, y=280, width=200)
+auth_no_entry.place(x=20, y=470, width=300)
+
+
+# contact first name
+
+first_name_label = ttk.Label(root, text='First Name:', foreground=text_color, font=('Ariel', 10))
+first_name_label.place(x=20, y=500)
+
+first_name_entry = ttk.Entry(root, textvariable=person_Fname)
+first_name_entry.place(x=20, y=520)
+
+
+# contact last name
+
+last_name_label = ttk.Label(root, text='Last Name:', foreground=text_color, font=('Ariel', 10))
+last_name_label.place(x=195, y=500)
+
+last_name_entry = ttk.Entry(root, textvariable=person_Lname)
+last_name_entry.place(x=195, y=520)
+
+
+# contact email
+
+email_label = ttk.Label(root, text='Email Address:', foreground=text_color, font=('Ariel', 10))
+email_label.place(x=20, y=550)
+
+email_entry = ttk.Entry(root, textvariable=person_email)
+email_entry.place(x=20, y=570, width=250)
+
+
+# contact mobile
+
+mobile_label = ttk.Label(root, text='Phone\Mobile number:', foreground=text_color, font=('Ariel', 10))
+mobile_label.place(x=20, y=600)
+
+mobile_entry = ttk.Entry(root, textvariable=person_mobile)
+mobile_entry.place(x=20, y=620)
 
 
 
