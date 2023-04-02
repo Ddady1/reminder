c2c7c5f897ab1f5827dc1f68aed1ce8c74839a73
Ddady1 <ddady1@gmail.com>
2023-04-02 12:53:27 +0300

    calendar

---
+++ calendar_picker.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkcalendar import Calendar
+from ctypes import windll
+from datetime import date
+
+windll.shcore.SetProcessDpiAwareness(1)
+
+text_color = 'DodgerBlue4'
+
+# getting todays date
+
+today = date.today()
+todaysdate = today.strftime('%d/%m/%Y')
+
+
+# Create the main window
+
+root = tk.Tk()
+root.title('Calendar')
+root.geometry('400x400+350+350')
+root.resizable(False, False)
+root.iconbitmap('assets/reminder.ico')
+
+
+# creating calendar obj
+
+cal = Calendar(root, selectmode='day', year=2023 ,month=4, date=2)
+cal.pack(pady=40)
+
+
+
+
+root.mainloop()
