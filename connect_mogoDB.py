564da82d0a2baef6cc66f6c052751c567af45162
Dady <ddady1@gmail.com>
2023-05-22 22:23:21 +0300

    mongodb

--- connect_mogoDB.py
+++ connect_mogoDB.py
@@ -1,8 +1,15 @@
 from pymongo import MongoClient
 
-con_string = 'mongodb+srv://myAtlasDBUser:<password>@myatlasclusteredu.vaenw0e.mongodb.net/?retryWrites=true&w=majority' #Replace password in string
+con_string = 'mongodb+srv://myAtlasDBUser:myatlas-001@myatlasclusteredu.vaenw0e.mongodb.net/?retryWrites=true&w=majority' #Replace password in string
 client = MongoClient(con_string)
 
 for db_name in client.list_database_names():
     print(db_name)
+
+    mydb = client[db_name]
+    for coll in mydb.list_collection_names():
+        print(coll)
+        for doc in coll.find({}):
+            print(doc)
+
 
